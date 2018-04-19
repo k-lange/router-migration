993db7bfc0b08334a678826b236e295e23bdc23e
Kolja Lange <mail@klange.org>
2018-04-19 16:57:35 +0200

    log router errors

--- src/index.js
+++ src/index.js
@@ -7,8 +7,13 @@ angular
         require('./angular/list').default,
         require('./angular/detail').default,
     ])
-    .config(['$locationProvider', enableHtml5Mode]);
+    .config(['$locationProvider', enableHtml5Mode])
+    .run(['$rootScope', logRouteErrors]);
 
 function enableHtml5Mode($locationProvider) {
     $locationProvider.html5Mode({ enabled: true });
+}
+
+function logRouteErrors($rootScope) {
+    $rootScope.$on('$stateChangeError', (...args) => console.error(...args));
 }
